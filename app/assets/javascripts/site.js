1c25b015aed3116a9f9b7e0c019ad0e2976aee9a
Rachel Krupnick <rachel@rachelkrupnick.com>
2015-12-14 20:37:15 -0500

    Ajax loading

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,4 +1,7 @@
 $(document).on('ready page:load', function() {
+	// Alert and Notices fade out
+	$('.alert').delay(1000).fadeOut(600);
+	// Raty Plugin
 	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating' });
 	$('.rated').raty({ path: '/assets',
 		readOnly: true,
@@ -6,4 +9,9 @@ $(document).on('ready page:load', function() {
 			return $(this).attr('data-score');
 		}
 	});
-});
+	// ElevateZoom Plugin
+	$('.img-zoom').elevateZoom({
+		zoomType: "inner",
+		cursor: "crosshair"
+	});
+}); //End Document Ready Function
